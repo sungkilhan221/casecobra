b86f8f46bed0f3bb2c67621baed1c2b740b17cab
Sung Kil Han <sungkilhan221@gmail.com>
2024-05-10 17:37:38 -0400

    chore: Update metadataBase URL in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -42,6 +42,6 @@ export function constructMetadata({
       creator: "@joshtriedcoding",
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/"),
+    metadataBase: new URL("https://casecobra-rosy.vercel.app/"),
   };
 }
